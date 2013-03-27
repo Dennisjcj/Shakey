2e3ecf60ad6fd1acecf309bb7d4d62b402a4fe58
oaktree <oaktree333@gmail.com>
2013-03-27 17:18:52 -0400

    Toast Message per Button

--- src/com/shakey/RemoteControlReceiver.java
+++ src/com/shakey/RemoteControlReceiver.java
@@ -2,15 +2,16 @@ package com.shakey;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 
 public class RemoteControlReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		
+		if(Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())){
+			Toast.makeText(context, "Received Intent.ACTION_MEDIA_BUTTON", Toast.LENGTH_SHORT).show();
+		}
 		
 	}
-	
-
 }
