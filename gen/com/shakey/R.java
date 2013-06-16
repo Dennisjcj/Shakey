9ae702a79b9c002197a3770bc036592e28c5c45d
Oaktree <oaktree333@gmail.com>
2013-06-15 21:54:05 -0400

    Revert "dern"
    This reverts commit 8bacfbd235e8a8e57d340a6450185d5f47352f99.

--- gen/com/shakey/R.java
+++ gen/com/shakey/R.java
@@ -15,30 +15,34 @@ public final class R {
         public static final int smileyface=0x7f020001;
     }
     public static final class id {
-        public static final int buttonenter=0x7f08000f;
-        public static final int buttonmenu=0x7f080006;
+        public static final int buttonenter=0x7f080011;
+        public static final int buttonmenu=0x7f080003;
         public static final int buttonmusic=0x7f080000;
-        public static final int buttonpause=0x7f080005;
-        public static final int buttonplay=0x7f080004;
-        public static final int editTextmili=0x7f08000e;
-        public static final int editTextmin=0x7f08000c;
-        public static final int manAuto=0x7f080001;
-        public static final int menu_settings=0x7f080017;
-        public static final int radioCustomVideo=0x7f080014;
-        public static final int radioGroup3=0x7f080007;
-        public static final int radioGroup4=0x7f080010;
-        public static final int radioauto=0x7f080003;
-        public static final int radiobanana=0x7f080011;
-        public static final int radiobubbles=0x7f080013;
-        public static final int radiofireworks=0x7f080012;
-        public static final int radiomanual=0x7f080002;
-        public static final int radionone=0x7f080015;
-        public static final int radiox=0x7f080008;
-        public static final int radioy=0x7f080009;
-        public static final int radioz=0x7f08000a;
-        public static final int textView1=0x7f08000b;
-        public static final int textView2=0x7f08000d;
-        public static final int video=0x7f080016;
+        public static final int buttonpause=0x7f080008;
+        public static final int buttonplay=0x7f080007;
+        public static final int editTextmili=0x7f080010;
+        public static final int editTextmin=0x7f08000e;
+        public static final int manAuto=0x7f080004;
+        public static final int menu_settings=0x7f08001b;
+        public static final int radioCustomVideo=0x7f080015;
+        public static final int radioGroup3=0x7f080019;
+        public static final int radioGroup4=0x7f08001a;
+        public static final int radioauto=0x7f080006;
+        public static final int radiobanana=0x7f080012;
+        public static final int radiobubbles=0x7f080014;
+        public static final int radiofireworks=0x7f080013;
+        public static final int radiomanual=0x7f080005;
+        public static final int radionone=0x7f080016;
+        public static final int radiox=0x7f08000a;
+        public static final int radioy=0x7f08000b;
+        public static final int radioz=0x7f08000c;
+        public static final int seekBar1=0x7f080002;
+        public static final int settings=0x7f080009;
+        public static final int textView1=0x7f08000d;
+        public static final int textView2=0x7f08000f;
+        public static final int textView5=0x7f080001;
+        public static final int video=0x7f080018;
+        public static final int y_axis=0x7f080017;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
